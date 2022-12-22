525a8697bae077468381e41486bd9bbb54552317
Michał Hewelt <mhewelt9@gmail.com>
2022-12-22 20:06:53 +0100

    add flight model

--- backend/flight_checker/models.py
+++ backend/flight_checker/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Flight(models.Model):
+    max_price = models.IntegerField()
+    location_from = models.CharField(max_length=30)
+    location_to = models.CharField(max_length=30)
+    email = models.EmailField()
+
+    def __str__(self):
+        return f"{self.email}: from {self.location_from} to {self.location_to}"
