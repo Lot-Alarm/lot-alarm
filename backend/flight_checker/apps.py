32ad620a4e850c8afaa55a83dfac62098dfc44f1
Michał Hewelt <mhewelt9@gmail.com>
2022-12-22 18:49:52 +0100

    initialize project v2

--- backend/flight_checker/apps.py
+++ backend/flight_checker/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class FlightCheckerConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
+    name = 'flight_checker'
